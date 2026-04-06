d0b69980a839dffc2b84f98522e175381ff6944d
Birabhadra <birabhadrasaho019@gmail.com>
2026-04-06 12:03:05 +0530

    Removing Book store api project

--- Book store api/routes/admin-router.js
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-const router=express.Router();
-const authMiddleware=require('../middlewares/auth-middleware')
-const adminMiddleware=require('../middlewares/role-middleware');
-
-router.get("/welcome",authMiddleware,adminMiddleware,(req,res)=>{
-    res.send("Welcome to the admin panel");
-});
-module.exports=router;
